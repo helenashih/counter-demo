6582c74a8988b227fa809f9b4c801b1d7f447f94
helenashih <42580758+helenashih@users.noreply.github.com>
2021-01-24 22:40:12 -0500

    Single source of truth and sync multiple components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,42 +1,32 @@
 import React, {Component} from 'react';
 
 class Counter extends Component{
-    state = {
-        count: 0
-        // tags: ['Test-tag1', 'Test-tag2', 'Test-tag3']
-    };
-    // constructor() {
-    //    super();
-    //    this.handleIncrement = this.handleIncrement.bind(this);
-    // }
-    handleIncrement = (product) => {
-        console.log(product)
-        this.setState({count: this.state.count + 1})
-    }
+
+
 
     render()
     {
         return (
             <div>
-                {/*
-                {this.state.tags.length === 0 && 'Please create a new tag!'}
-                {this.renderTags()}
-                */}
-            <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
-            <button onClick={ () => this.handleIncrement({id: 1})}
-                    className="btn btn-primary">Increment</button>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button onClick={() => this.props.onIncrement(this.props.counter)}
+                    className="btn btn-primary">Increment
+            </button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)}
+                        className="btn btn-danger btn-sm m-2">Delete
+                </button>
     </div>
     );
     }
     getBadgeClasses() {
         let classes = "badge m-2 bg-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.props.counter.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const {count} = this.state;
-        return count === 0 ? "Zero" : count;
+        const { value }= this.props.counter;
+        return value === 0 ? "Zero" : value;
     }
 }
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from './components/counter';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
