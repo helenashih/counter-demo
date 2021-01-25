f5f7e683bcff1d7fc768bff39ef4ce3c998ae4c0
helenashih <42580758+helenashih@users.noreply.github.com>
2021-01-25 10:37:02 -0500

    Added stateless function, lifecycle hooks, mounting and unmounting as well as updates.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
 
 class Counter extends Component{
+    componentDidUpdate(prevProps, prevState) {
+        console.log('prevProps', prevProps);
+        console.log('prevState', prevState);
+        if (prevProps.counter.value !== this.props.counter.value) {
+            //Ajax call and get new data from server
+            console.log('Counter changed');
+        }
+    }
 
-
-
+    componentWillUnmount() {
+        console.log('Counter - Unmount');
+        // clean up memory
+    }
     render()
     {
+        console.log('Counter rendered');
         return (
             <div>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 
-class NavBar extends Component {
-    render() {
-        return(
+const NavBar = ({totalCounters}) => {
+    console.log('Navbar Rendered');
+    return(
         <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#">
+                Navbar <span className="badge badge-pill badge-secondary">{totalCounters}</span>
+            </a>
         </nav>
-);
-    }
-}
+    );
+};
+
 export default NavBar;
 
